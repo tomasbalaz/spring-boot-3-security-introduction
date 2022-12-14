c2f7e4724a59b5c68dd41bb6e7af89cf745dc046
Tomas Balaz <balaztomas0@gmail.com>
2022-12-14 14:13:09 +0100

    Created spring-boot-3-security-introduction

---
+++ src/main/java/sk/balaz/springboot3securityintroduction/SpringBoot3SecurityIntroductionApplication.java
@@ -0,0 +1,13 @@
+package sk.balaz.springboot3securityintroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3SecurityIntroductionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3SecurityIntroductionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sk/balaz/springboot3securityintroduction/SpringBoot3SecurityIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.balaz.springboot3securityintroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3SecurityIntroductionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
