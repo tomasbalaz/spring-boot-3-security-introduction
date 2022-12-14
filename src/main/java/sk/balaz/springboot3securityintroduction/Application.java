3d24a94aae61d8356c4a4676326242a2bbdeaba0
Tomas Balaz <balaztomas0@gmail.com>
2022-12-14 14:20:35 +0100

    Exposed  REST GET /api/v1/greetings, /api/v1/greetings/say-goodbye

--- src/main/java/sk/balaz/springboot3securityintroduction/Application.java
+++ src/main/java/sk/balaz/springboot3securityintroduction/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBoot3SecurityIntroductionApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBoot3SecurityIntroductionApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
---
+++ src/main/java/sk/balaz/springboot3securityintroduction/GreetingController.java
@@ -0,0 +1,21 @@
+package sk.balaz.springboot3securityintroduction;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/greetings")
+public class GreetingController {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Hello from Spring Security introduction");
+    }
+
+    @GetMapping("/say-goodbye")
+    public ResponseEntity<String> sayGoodBye() {
+        return ResponseEntity.ok("Good bye and see you later");
+    }
+}
