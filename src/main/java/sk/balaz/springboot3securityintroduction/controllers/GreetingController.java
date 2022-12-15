c385c928b3a886a55485e947f106c56ff022567c
Tomas Balaz <balaztomas0@gmail.com>
2022-12-15 12:16:24 +0100

    Enabled HTTP Basic authentication

--- src/main/java/sk/balaz/springboot3securityintroduction/controllers/GreetingController.java
+++ src/main/java/sk/balaz/springboot3securityintroduction/controllers/GreetingController.java
@@ -1,4 +1,4 @@
-package sk.balaz.springboot3securityintroduction;
+package sk.balaz.springboot3securityintroduction.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
