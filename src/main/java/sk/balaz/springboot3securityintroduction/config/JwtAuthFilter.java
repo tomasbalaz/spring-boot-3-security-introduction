f6036264a3f14044914eb7873bd6f10dab9d46de
Tomas Balaz <balaztomas0@gmail.com>
2023-03-08 15:43:17 +0100

    Continue in flow when user has not been authenticated

--- src/main/java/sk/balaz/springboot3securityintroduction/config/JwtAuthFilter.java
+++ src/main/java/sk/balaz/springboot3securityintroduction/config/JwtAuthFilter.java
@@ -5,28 +5,49 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
+@AllArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final UserDetailsService userDetailsService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+        HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
         String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
         String userEmail;
         String jwtToken;
 
-        if(authHeader == null || !authHeader.startsWith("Bearer")) {
+        if (authHeader == null || !authHeader.startsWith("Bearer")) {
             filterChain.doFilter(request, response);
             return;
         }
         jwtToken = authHeader.substring(HttpHeaders.AUTHORIZATION.length());
-        // userEmail = TODO will be implemented
+        userEmail = "something"; //TODO will be implemented
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (userEmail != null && authentication == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
+            boolean isTokenValid = false; //TODO will be implemented
+
+            if (isTokenValid) {
+                UsernamePasswordAuthenticationToken token =
+                    new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(token);
+            }
+        }
     }
 }
