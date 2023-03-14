10db8e3b4af0f2b3b1a5af0e1a7c3fd5b7701f0b
Tomas Balaz <balaztomas0@gmail.com>
2023-03-14 14:41:10 +0100

    Used JwtAuthFilter in SecurityConfig

--- src/main/java/sk/balaz/springboot3securityintroduction/config/SecurityConfig.java
+++ src/main/java/sk/balaz/springboot3securityintroduction/config/SecurityConfig.java
@@ -1,22 +1,27 @@
 package sk.balaz.springboot3securityintroduction.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
+            .anyRequest()
+            .authenticated()
+            .and()
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
